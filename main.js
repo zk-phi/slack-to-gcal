c6892ef200be11c4bdca32c47858aa0db19cb418
zk-phi <zk-phi@users.noreply.github.com>
2020-01-23 21:11:37 +0900

    Add feature to delete events interactively

--- main.js
+++ main.js
@@ -57,54 +57,100 @@ function formatEvent (event) {
     var to = event.getEndTime();
     to.setDate(to.getDate() - 1);
 
-    return "'" + event.getTitle() + "'" + (
+    var text = "- *" + event.getTitle() + "*" + (
         from < to ? (
             " from " + from.toLocaleDateString() + " to " + to.toLocaleDateString()
         ) : (
             " at " + from.toLocaleDateString()
         )
     );
+
+    return {
+        type: "section",
+        text: { type: "mrkdwn", text: text },
+        accessory: {
+            type: "button",
+            text: {type: "plain_text", text: "Delete", emoji: false },
+            value: "delete:" + event.getId()
+        }
+    };
 }
 
 /* --- slack utils */
 
-function postToSlack (text) {
+function postToSlack (text, blocks) {
     return UrlFetchApp.fetch(properties.getProperty("SLACK_WEBHOOK_URL"), {
         method: 'post',
         contentType: 'application/json',
-        payload: JSON.stringify({ text: text })
+        payload: JSON.stringify({ text: text || "", blocks: blocks || [] })
     });
 }
 
 /* --- interface */
 
-function doAddEvent (e) {
-    var res = addEventByStr(e.parameter.text);
-    postToSlack("ADDED EVENT: '" + formatEvent(res));
+function doAddEvent (params) {
+    var res = addEventByStr(params.text);
+    postToSlack("", [
+        {
+            type: "section",
+            text: { type: "mrkdwn", text: ":white_check_mark: *EVENT ADDED* :white_check_mark:" },
+        },
+        formatEvent(res)
+    ]);
 }
 
-function doListEvent (e) {
+function doListEvent () {
     var events = getEventList();
-    postToSlack("UPCOMING EVENTS: \n" + events.map(formatEvent).join("\n"));
+    postToSlack("", [
+        {
+            type: "section",
+            text: { type: "mrkdwn", text: ":calendar: *UPCOMING EVENTS* :calendar:" },
+        }
+    ].concat(events.map(formatEvent)));
 }
 
-function doHelp (e) {
+function doHelp () {
     postToSlack(
+        "USAGE\n" +
         "- `/task hogehoge [yyyy/]mm/dd[-dd]` to add events\n" +
         "- `/task list` to see all events"
     );
 }
 
+function doAction (params) {
+    var match = params.actions[0].value.match(/^([^:]+):(.*)$/);
+    if (!match) throw "Parse error";
+
+    var event = CalendarApp.getEventById(match[2]);
+
+    if (match[1] == "delete") {
+        postToSlack("", [
+            {
+                type: "section",
+                text: { type: "mrkdwn", text: ":wastebasket: *EVENT DELETED:*" },
+            },
+            formatEvent(event)
+        ]);
+        event.deleteEvent();
+    } else {
+        throw "Unknown action";
+    }
+}
+
 function doPost (e) {
-    var verificationToken = e.parameter.token;
+    var params = e.parameter.payload ? JSON.parse(e.parameter.payload) : e.parameter;
+
+    var verificationToken = params.token;
     if (verificationToken != properties.getProperty("SLACK_VERIFICATION_TOKEN")) throw "Invalid token";
 
-    if (e.parameter.text == '') {
-        doHelp(e);
-    } else if (e.parameter.text == 'list') {
-        doListEvent(e);
+    if (params.actions) {
+        doAction(params);
+    } else if (params.text == '') {
+        doHelp();
+    } else if (params.text == 'list') {
+        doListEvent();
     } else {
-        doAddEvent(e);
+        doAddEvent(params);
     }
 
     return ContentService.createTextOutput("");
